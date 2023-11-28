2376c24b23444d36324beb4837725cb66727e926
vj146 <121240553+vj146@users.noreply.github.com>
2023-11-28 19:05:28 +0000

    Removed post operations

--- app/pages/dashboard/page.jsx
+++ app/pages/dashboard/page.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import UserInfo from '../../components/UserInfo'
-import Post from '../../components/Post'
-import PostInfo from '../../components/PostInfo'
 
 export default function Dashboard() {
   return (
     <>
       <UserInfo />
-
-      <div className='container mx-auto my-12'>
-        <div><Post /></div>
-        <div><PostInfo /></div>        
-      </div>
     </>
   )
 }
