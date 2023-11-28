c40e8dcd9b2331d6f55bdfeb743fa734dc82da70
vj146 <121240553+vj146@users.noreply.github.com>
2023-11-28 21:17:42 +0000

    Fixed callback url

--- app/pages/register/page.jsx
+++ app/pages/register/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import RegisterForm from '../../components/RegisterForm'
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { authOptions } from '@/lib/authOptions';
 
 export default async function register() {
   const session = await getServerSession(authOptions);
